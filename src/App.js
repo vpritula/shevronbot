ef77b00da2b661376053ac7a831d5052662c4937
Volodymyr <volodymyr@admins-MacBook-Pro-2.local>
2023-05-07 13:33:11 +0300

    telegram hook

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 
 function App() {
-  
+
   const {onToggleButton, tg} = useTelegram();
 
-  useEffect(() => {
-      tg.ready();
-  }, [])
+//  useEffect(() => {
+//      tg.ready();
+//  }, [])
 
   return (
       <div className="App">
